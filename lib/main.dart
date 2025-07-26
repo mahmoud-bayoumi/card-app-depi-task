1a3b34f7245cbaa45f243bd877b56f0de447e490
mahmoud-bayoumi <mahmoudbayoumi110@gmail.com>
2025-07-26 21:54:32 +0300

    update app name

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,11 @@ import 'package:first_depi_app/depi_first_app_view.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const DepiFirstApp());
+  runApp(const SelfCardApp());
 }
 
-class DepiFirstApp extends StatelessWidget {
-  const DepiFirstApp({super.key});
+class SelfCardApp extends StatelessWidget {
+  const SelfCardApp({super.key});
 
   @override
   Widget build(BuildContext context) {
